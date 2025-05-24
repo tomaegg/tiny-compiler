b1966e46a37c843a7153bbc665af040f289c4e37
scarlet <phantom190514@gmail.com>
2025-05-24 19:57:37 +0800

    feat: add stack utils

---
+++ utils/stack/stack.go
@@ -0,0 +1,24 @@
+package stack
+
+type Stack[T any] struct {
+	keys []T
+}
+
+func (stack *Stack[T]) Push(key T) {
+	stack.keys = append(stack.keys, key)
+}
+
+func (stack *Stack[T]) Top() T {
+	x := stack.keys[len(stack.keys)-1]
+	return x
+}
+
+func (stack *Stack[T]) Pop() T {
+	var x T
+	x, stack.keys = stack.keys[len(stack.keys)-1], stack.keys[:len(stack.keys)-1]
+	return x
+}
+
+func (stack *Stack[T]) IsEmpty() bool {
+	return len(stack.keys) == 0
+}
